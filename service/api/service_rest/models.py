9b4003bd72bf91cdbf4b3f7b75be7b7d2366564b
robbie <rtambunting@gmail.com>
2023-04-24 16:21:01 -0700

    Add appointment model

--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -4,3 +4,11 @@ class Technician(models.Model):
     first_name = models.CharField(max_length=150)
     last_name = models.CharField(max_length=150)
     employee_id = models.PositiveIntegerField(unique=True)
+
+class Appointment(models.Model):
+    date_time = models.DateTimeField()
+    reason = models.TextField()
+    status = models.CharField(max_length=150)
+    vin = models.CharField(max_length=150, unique=True)
+    customer = models.CharField(max_length=150)
+    technician = models.ForeignKey(Technician, related_name="appointment", on_delete=models.CASCADE)
