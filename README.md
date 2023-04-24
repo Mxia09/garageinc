5360402cb6ce4fc005c4be128ede97c1306c8596
Marvin Xia <marvinxia09@gmail.com>
2023-04-24 16:24:12 -0700

    installed_apps updated with sales_rest and models created

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Team:
 
-* Person 1 - Which microservice?
+* Marvin Xia - Sales 
 * Person 2 - Which microservice?
 
 ## Design
--- sales/api/sales_rest/models.py
+++ sales/api/sales_rest/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+class Salesperson(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    employee_id = models.CharField(max_length=200)
+    
+class Customer(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=200)
+
+class AutomobileVO(models.Model):
+    vin = models.CharField(max_length=50) 
+    
+class Sale(models.Model):
+    automobile = models.ForeignKey(
+        AutomobileVO, 
+        related_name="Sale",
+        on_delete=models.CASCADE,
+    )
+    
+    salesperson = models.ForeignKey(
+        Salesperson, 
+        related_name="Sale",
+        on_delete=models.CASCADE,
+    )    
+
+    customer = models.ForeignKey(
+        Customer, 
+        related_name="Sale",
+        on_delete=models.CASCADE,
+    )        
+    
+    price = models.PositiveBigIntegerField()
+    
+
+    # Create your models here.
