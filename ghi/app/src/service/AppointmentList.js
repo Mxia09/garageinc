f281f9984c657662f509c9b50ba35e490bb7ce82
robbie <rtambunting@gmail.com>
2023-04-25 13:55:42 -0700

    Filter out canceled and finished tasks from AppointmentList

--- ghi/app/src/service/AppointmentList.js
+++ ghi/app/src/service/AppointmentList.js
@@ -9,7 +9,10 @@ export const AppointmentList = () => {
       const response = await fetch(appointmentsUrl);
       if (response.ok) {
         const responseData = await response.json();
-        setAppointments(responseData.appointments);
+        const appointments = responseData.appointments;
+        setAppointments(
+          appointments.filter((appt) => appt.status === "created")
+        );
       }
     }
     getAppointments();
@@ -25,9 +28,7 @@ export const AppointmentList = () => {
     const response = await fetch(cancelUrl, fetchConfig);
     if (response.ok) {
       alert("Appointment Updated!");
-      //   const appt = await response.json();
-      //   setAppointments(appointments.filter((appt) => appt.status === "created"));
-      //   console.log(appointments.filter((appt) => appt.status === "created"));
+      setAppointments(appointments.filter((appt) => appt.status === "created"));
     }
   };
 
@@ -42,48 +43,55 @@ export const AppointmentList = () => {
     if (response.ok) {
       alert("Appointment Updated!");
       const appt = await response.json();
-      console.log(appt);
+      setAppointments(appointments.filter((appt) => appt.status === "created"));
     }
   };
 
   return (
-    <table className="table table-striped">
-      <thead>
-        <tr>
-          <th>VIN</th>
-          <th>Customer</th>
-          <th>Date + time</th>
-          <th>Technician</th>
-          <th>Reason</th>
-        </tr>
-      </thead>
-      <tbody>
-        {appointments.map((appointment) => {
-          return (
-            <tr>
-              <td>{appointment.vin}</td>
-              <td>{appointment.customer}</td>
-              <td>{appointment.date_time}</td>
-              <td>{appointment.technician.first_name}</td>
-              <td>{appointment.reason}</td>
-              <td>
-                <button
-                  className="btn btn-danger"
-                  onClick={() => handleCancel(appointment.id)}
-                >
-                  Cancel
-                </button>
-                <button
-                  className="btn btn-success"
-                  onClick={() => handleFinish(appointment.id)}
-                >
-                  Finish
-                </button>
-              </td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+    <>
+      <h1>Appointments</h1>
+      <p>
+        NOTE: If your appointment is canceled or finished, it will NOT show up
+        on this list
+      </p>
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th>VIN</th>
+            <th>Customer</th>
+            <th>Date + time</th>
+            <th>Technician</th>
+            <th>Reason</th>
+          </tr>
+        </thead>
+        <tbody>
+          {appointments.map((appointment) => {
+            return (
+              <tr>
+                <td>{appointment.vin}</td>
+                <td>{appointment.customer}</td>
+                <td>{appointment.date_time}</td>
+                <td>{appointment.technician.first_name}</td>
+                <td>{appointment.reason}</td>
+                <td>
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => handleCancel(appointment.id)}
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    className="btn btn-success"
+                    onClick={() => handleFinish(appointment.id)}
+                  >
+                    Finish
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
   );
 };
