d479795c08f6774047fd7737347a30fcdddee25a
robbie <rtambunting@gmail.com>
2023-04-25 09:55:10 -0700

    Finish appointments form

--- ghi/app/src/service/AppointmentList.js
+++ ghi/app/src/service/AppointmentList.js
@@ -1,5 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export const AppointmentList = () => {
-  return <div>AppointmentList</div>;
+  const [appointments, setAppointments] = useState([]);
+  useEffect(() => {
+    async function getAppointments() {
+      const appointmentsUrl = "http://localhost:8080/api/appointments/";
+      const response = await fetch(appointmentsUrl);
+      if (response.ok) {
+        const responseData = await response.json();
+        setAppointments(responseData.appointments);
+      }
+      console.log(appointments);
+    }
+    getAppointments();
+  }, []);
+
+  return (
+    <table className="table table-striped">
+      <thead>
+        <tr>
+          <th>VIN</th>
+          <th>Customer</th>
+          <th>Date + time</th>
+          <th>Technician</th>
+          <th>Reason</th>
+        </tr>
+      </thead>
+      <tbody></tbody>
+    </table>
+  );
 };
