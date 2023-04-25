8832208aa97039370ecb89b1d2ef7261a1ebd0ca
robbie <rtambunting@gmail.com>
2023-04-25 14:29:07 -0700

    Add year to AutoList

--- ghi/app/src/automobile/AutoList.js
+++ ghi/app/src/automobile/AutoList.js
@@ -23,6 +23,7 @@ export const AutoList = () => {
           <tr>
             <th>VIN</th>
             <th>Color</th>
+            <th>Year</th>
             <th>Model</th>
             <th>Manufacturer</th>
             <th>Sold</th>
@@ -34,6 +35,7 @@ export const AutoList = () => {
               <tr>
                 <td>{auto.vin}</td>
                 <td>{auto.color}</td>
+                <td>{auto.year}</td>
                 <td>{auto.model.name}</td>
                 <td>{auto.model.manufacturer.name}</td>
                 <td>{auto.sold ? "Yes" : "No"}</td>
